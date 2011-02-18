cd3425546238bd934f7db486f43ff95551d2e9ad
Regan <rroyste2@mail.umw.edu>
2011-02-17 20:22:25 -0500

    Added another table. Something is off in general_info table though..

--- themeparks.sql
+++ themeparks.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2011 at 07:38 PM
+-- Generation Time: Feb 17, 2011 at 08:20 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS `general_info` (
   `City` varchar(20) DEFAULT NULL,
   `State` char(2) DEFAULT NULL,
   `Zipcode` varchar(10) DEFAULT NULL,
-  `adult_price` decimal(3,2) DEFAULT NULL,
-  `children_price` decimal(3,2) DEFAULT NULL,
+  `adult_price` decimal(5,2) DEFAULT NULL,
+  `children_price` decimal(5,2) DEFAULT NULL,
   `Phone_Number` varchar(13) DEFAULT NULL,
   `Park_Name` varchar(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -41,14 +41,14 @@ CREATE TABLE IF NOT EXISTS `general_info` (
 
 INSERT INTO `general_info` (`City`, `State`, `Zipcode`, `adult_price`, `children_price`, `Phone_Number`, `Park_Name`) VALUES
 ('Doswell', 'VA', '23047', '9.99', '9.99', '800-876-5338', NULL),
-(NULL, NULL, NULL, NULL, NULL, NULL, 'Kings Dominion'),
-('Williamsburg', 'VA', '23185', '9.99', '9.99', '800-343-7946', 'Busch Gardens'),
-('Sandusky', 'OH', '44870', '9.99', '9.99', '419-627-2350', 'Cedar Point'),
-('Orlando', 'FL', '32819', '9.99', '9.99', '407-363-8000', 'Islands of Adventure'),
-('Orlando', 'FL', '32819', '9.99', '9.99', '407-363-8000', 'Universal Studios'),
-('Santa Claus', 'IN', '47579', '9.99', '9.99', '812-937-4401', 'Holiday World'),
-('Mason', 'OH', '45040', '9.99', '9.99', '513-754-5700', 'Kings Island'),
-('Jackson', 'NJ', '08527', '9.99', '9.99', '732-928-1821', 'Six Flags Great Adve');
+(NULL, NULL, NULL, '42.99', '34.99', NULL, 'Kings Dominion'),
+('Williamsburg', 'VA', '23185', '63.99', '53.99', '800-343-7946', 'Busch Gardens'),
+('Sandusky', 'OH', '44870', '46.99', '21.00', '419-627-2350', 'Cedar Point'),
+('Orlando', 'FL', '32819', '82.00', '74.00', '407-363-8000', 'Islands of Adventure'),
+('Orlando', 'FL', '32819', '82.00', '74.00', '407-363-8000', 'Universal Studios'),
+('Santa Claus', 'IN', '47579', '42.95', '32.95', '812-937-4401', 'Holiday World'),
+('Mason', 'OH', '45040', '51.99', '51.99', '513-754-5700', 'Kings Island'),
+('Jackson', 'NJ', '08527', '59.99', '34.99', '732-928-1821', 'Six Flags Great Adve');
 
 -- --------------------------------------------------------
 
@@ -87,6 +87,27 @@ INSERT INTO `rollercoaster` (`Name`, `Park`, `Type`, `Speed_mph`, `Height_ft`, `
 ('The Voyage', 'Holiday World', 'Wooden', 67, 173, 2),
 ('The Raven', 'Holiday World', 'Wooden', 50, 110, 1),
 ('The Legend', 'Holiday World', 'Wooden', 65, 116, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `waterrides`
+--
+
+CREATE TABLE IF NOT EXISTS `waterrides` (
+  `Name` varchar(20) DEFAULT NULL,
+  `Park` varchar(20) DEFAULT NULL,
+  `Length_min` decimal(5,2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `waterrides`
+--
+
+INSERT INTO `waterrides` (`Name`, `Park`, `Length_min`) VALUES
+('Shenandoah Log Flume', 'Kings Dominion', '3.40'),
+('Escape from Pompeii', 'Busch Gardens', '2.10'),
+('Thunder Canyon', 'Cedar Point', '2.30');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
