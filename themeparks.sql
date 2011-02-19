57baba27d438bccc5a08357b01e85b883c0826fb
Gary M <gmills@mail.umw.edu>
2011-02-19 17:24:25 -0500

    trying to push updated DB

--- themeparks.sql
+++ themeparks.sql
@@ -3,13 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2011 at 08:20 PM
+-- Generation Time: Feb 19, 2011 at 05:20 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-CREATE DATABASE IF NOT EXISTS themeparks;
-use themeparks;
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,6 +18,8 @@ use themeparks;
 --
 -- Database: `themeparks`
 --
+CREATE DATABASE `themeparks` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `themeparks`;
 
 -- --------------------------------------------------------
 
@@ -49,7 +50,8 @@ INSERT INTO `general_info` (`City`, `State`, `Zipcode`, `adult_price`, `children
 ('Orlando', 'FL', '32819', '82.00', '74.00', '407-363-8000', 'Universal Studios'),
 ('Santa Claus', 'IN', '47579', '42.95', '32.95', '812-937-4401', 'Holiday World'),
 ('Mason', 'OH', '45040', '51.99', '51.99', '513-754-5700', 'Kings Island'),
-('Jackson', 'NJ', '08527', '59.99', '34.99', '732-928-1821', 'Six Flags Great Adve');
+('Jackson', 'NJ', '08527', '59.99', '34.99', '732-928-1821', 'Six Flags Great Adve'),
+('Tampa', 'Fl', '33674', '77.99', '69.99', '888-800-5359', 'Busch Gardens');
 
 -- --------------------------------------------------------
 
@@ -98,17 +100,18 @@ INSERT INTO `rollercoaster` (`Name`, `Park`, `Type`, `Speed_mph`, `Height_ft`, `
 CREATE TABLE IF NOT EXISTS `waterrides` (
   `Name` varchar(20) DEFAULT NULL,
   `Park` varchar(20) DEFAULT NULL,
-  `Length_min` decimal(5,2) DEFAULT NULL
+  `Length_min` decimal(5,2) DEFAULT NULL,
+  `Height_ft` int(4) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `waterrides`
 --
 
-INSERT INTO `waterrides` (`Name`, `Park`, `Length_min`) VALUES
-('Shenandoah Log Flume', 'Kings Dominion', '3.40'),
-('Escape from Pompeii', 'Busch Gardens', '2.10'),
-('Thunder Canyon', 'Cedar Point', '2.30');
+INSERT INTO `waterrides` (`Name`, `Park`, `Length_min`, `Height_ft`) VALUES
+('Shenandoah Log Flume', 'Kings Dominion', '3.40', NULL),
+('Escape from Pompeii', 'Busch Gardens', '2.10', NULL),
+('Thunder Canyon', 'Cedar Point', '2.30', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
