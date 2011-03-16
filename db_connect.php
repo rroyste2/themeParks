8f7232f5ba7ddb27eaacab33f37f4e21222b168c
mikeyGlitz <mglitzos@mail.umw.edu>
2011-03-16 15:25:49 -0400

    I added a new user called themeguru for seurity purposes. Only the server admin should have the ability to use root.
    What you have to do is go into phpmyadmin and add a new user under Privileges
    username: themeguru
    password: parks
    
    The next screen should ask for permissions select under data:
    SELECT
    INSERT
    UPDATE
    DELETE
    
    After doing all that, you should be good to go. I have it set so that themguru cannot create, alter, drop or do anything with the tables. themeguru's job is to add/remove new data to the table, not manipulate them in any way.

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'root', 'Peter804', 'themeparks')
+$db = mysqli_connect('localhost', 'themeguru', 'parks', 'themeparks')
 
 or die ("ERROR: connecting to mysql server!");
 ?>
